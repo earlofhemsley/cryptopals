3967e4c9371a55bcb0c8dead5566a5cb86a5554c
Landon Hemsley <landonhemsley@gmail.com>
2021-06-09 07:57:56 -0600

    c27 -- make a compromise and parse a stringify'd array, catch it with regex, use a random key, and write up via javadoc

--- src/main/java/cryptopals/exceptions/InvalidPlaintextByteException.java
+++ src/main/java/cryptopals/exceptions/InvalidPlaintextByteException.java
@@ -1,34 +1,33 @@
 package cryptopals.exceptions;
 
-import lombok.Getter;
-import lombok.Setter;
+import java.util.Arrays;
 
 /**
  * This class is intended to be thrown if a message contains what is deemed ot be
  * an invalid character.
  */
-@Getter
-@Setter
 public class InvalidPlaintextByteException extends RuntimeException {
-
-    /**
-     * it's super insecure to make the original message accessible,
-     * but that's sort of the point of the exercise, so I'm rolling with it
-     */
-    private byte[] plainTextBytes;
 
     public InvalidPlaintextByteException(String message, byte[] plainTextBytes) {
         super(message);
-        this.plainTextBytes = plainTextBytes;
     }
 
     private static final byte LOWER_BOUND = ' ';
     private static final byte UPPER_BOUND = '~';
+
+    /**
+     * if an invalid byte shows up in a subject, then stringify that subject and throw an exception
+     *
+     * normally I would use new String(subject), but java doesn't preserve message bytes as part of String::new,
+     * Arrays::toString was a compromise to retain the byte values without just offering the bytes themselves
+     *
+     * @param subject plaintext byte array
+     */
     public static void throwIfContainsInvalidCharacter(byte[] subject) {
         for (byte c : subject) {
             if (c < LOWER_BOUND || c > UPPER_BOUND) {
                 throw new InvalidPlaintextByteException(String.format("%s contains an invalid character: %s",
-                        new String(subject), c), subject);
+                        Arrays.toString(subject), c), subject);
             }
         }
     }
--- src/test/java/cryptopals/challenges/sec04/C27.java
+++ src/test/java/cryptopals/challenges/sec04/C27.java
@@ -1,6 +1,7 @@
 package cryptopals.challenges.sec04;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import cryptopals.exceptions.InvalidPlaintextByteException;
@@ -8,7 +9,11 @@ import cryptopals.tool.XOR;
 import cryptopals.tool.sec04.C27_SameKeyIVAdminRightsOracle;
 import cryptopals.utils.ByteArrayUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 /**
  * Recover the key from CBC with IV=Key
@@ -43,11 +48,38 @@ public class C27 {
 
     private static final int BLOCK_LENGTH = 16;
 
-    private static final byte[] KEY = new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    private static final byte[] KEY = ByteArrayUtil.randomBytes(16);
     private final C27_SameKeyIVAdminRightsOracle oracle = new C27_SameKeyIVAdminRightsOracle(KEY);
 
+    @Test
+    void ensureUntamperedMessageWorks() {
+        final String msg = ";admin=true;A=AA";
+        var enc = oracle.padAndEncrypt(msg);
+        assertFalse(oracle.findAdminInCipherText(enc));
+    }
+
     /**
+     * complete the challenge
      *
+     * this specifically takes advantage of the decryption algorithm.
+     *
+     * in order to recover the key, (since the key and the iv are the same), you need to XOR an
+     * encrypted first block against a plaintext first block
+     *
+     * getting the plaintext first block is easy. decrypt normally.
+     * getting the encrypted first block is less obvious. to get the encrypted first block, you
+     * need to take advantage of the cbc algorithm.
+     *
+     * cbc, when decrypting, will take an encrypted block and xor it against the next block after that next
+     * block has been ecb decrypted to reveal the plaintext.
+     *
+     * so, if you overwrite the second block of ciphertext with all zeros and overwrite the third block of
+     * ciphertext with the first block of ciphertext, the algorithm will ecb decrypt that third block (which is really
+     * the first block) and then xor it against all 0s, which leaves that ecb decrypted third block unaltered
+     * (since an xor against 0 does nothing). And voila! You've got your two components: the ecb decrypted first
+     * block, and your plaintext first block.
+     *
+     * When you xor these two, you get the IV, which in this challenge, is also the key.
      */
     @Test
     void doTheChallenge() {
@@ -64,12 +96,26 @@ public class C27 {
         try {
             oracle.findAdminInCipherText(enc);
         } catch (InvalidPlaintextByteException ex) {
-            final byte[] pp = ex.getPlainTextBytes();
-            final byte[] pp1 = ByteArrayUtil.sliceByteArray(pp, 0, BLOCK_LENGTH);
-            final byte[] pp3 = ByteArrayUtil.sliceByteArray(pp, BLOCK_LENGTH*2, BLOCK_LENGTH);
-            //this should yield the key
-            final byte[] derivedKey = new XOR().multiByteXOR(pp1, pp3);
-            assertArrayEquals(KEY, derivedKey);
+            final Pattern p = Pattern.compile("^\\[([\\d, -]+)] contains an invalid character: [\\d-]+$");
+            var m = p.matcher(ex.getMessage());
+            if (m.find()) {
+                var splits = m.group(1).split(", ");
+
+                //pp = p prime, or plaintaxt prime
+                var pp = ArrayUtils.toPrimitive(Arrays.stream(splits).map(Byte::parseByte).toArray(Byte[]::new));
+
+                //block one (plaintext fully decrypted)
+                final byte[] pp1 = ByteArrayUtil.sliceByteArray(pp, 0, BLOCK_LENGTH);
+
+                //block three (ciphertext block one ecb decrypted and xor'd against 0)
+                final byte[] pp3 = ByteArrayUtil.sliceByteArray(pp, BLOCK_LENGTH*2, BLOCK_LENGTH);
+
+                //xor the two to get the key
+                final byte[] derivedKey = new XOR().multiByteXOR(pp1, pp3);
+                assertArrayEquals(KEY, derivedKey);
+            } else {
+                fail("couldn't find the byte array plaintext");
+            }
         }
     }
 }
