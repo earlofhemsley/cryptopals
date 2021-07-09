319d06b547cc7474f87834fe9d07efdac5776a44
Landon Hemsley <landonhemsley@gmail.com>
2021-07-09 07:05:29 -0600

    c32 -- restore the thread count to 7 to see if it breaks

--- src/test/java/cryptopals/challenges/sec04/C32.java
+++ src/test/java/cryptopals/challenges/sec04/C32.java
@@ -37,7 +37,7 @@ public class C32 {
      */
     @Test
     void completeTheChallenge() throws ExecutionException, InterruptedException {
-        final C31_32_TimingLeakExploiter exploiter = new C31_32_TimingLeakExploiter(FILE, port, restTemplate.getRestTemplate(), 3);
+        final C31_32_TimingLeakExploiter exploiter = new C31_32_TimingLeakExploiter(FILE, port, restTemplate.getRestTemplate(), 7);
 
         //start with a cheat set
         byte[] forgedHash = getCheatBytes();
