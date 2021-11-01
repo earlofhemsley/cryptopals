c8072c735428fb3ad5e0a24917bad9e8c609810a
Landon Hemsley <landonhemsley@gmail.com>
2021-11-01 07:11:38 -0600

    c40 -- get the rooting function working

--- src/main/java/cryptopals/utils/MathUtil.java
+++ src/main/java/cryptopals/utils/MathUtil.java
@@ -1,11 +1,18 @@
 package cryptopals.utils;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.math.BigInteger.ONE;
+import static java.math.BigInteger.ZERO;
+
+import com.google.common.base.Preconditions;
 import lombok.experimental.UtilityClass;
 
 import java.math.BigInteger;
 
 @UtilityClass
 public class MathUtil {
+    private static final BigInteger TWO = BigInteger.valueOf(2);
+    private static final BigInteger THREE = BigInteger.valueOf(3);
 
     /**
      * find the modular inverse of a mod n, which we call t
@@ -19,8 +26,10 @@ public class MathUtil {
      * @return the result, which I am calling t
      */
     public BigInteger invMod(final BigInteger a, final BigInteger n) {
+        checkArgument(!n.mod(a).equals(ZERO), "n mod a cannot equal zero");
+
         BigInteger t = BigInteger.ZERO;
-        BigInteger nextT = BigInteger.ONE;
+        BigInteger nextT = ONE;
         BigInteger r = n;
         BigInteger nextR = a;
 
@@ -35,8 +44,8 @@ public class MathUtil {
             nextR = tempR.subtract(q.multiply(nextR));
         }
 
-        if (r.compareTo(BigInteger.ONE) > 0) {
-            throw new ArithmeticException("a is not invertible");
+        if (r.compareTo(ONE) > 0) {
+            throw new ArithmeticException(String.format("%s not invertible. n mod a = %s", a, n.mod(a)));
         }
 
         if (t.compareTo(BigInteger.ZERO) < 0) {
@@ -44,5 +53,27 @@ public class MathUtil {
         }
 
         return t;
+    }
+
+    public BigInteger iroot(int k, int n) {
+        return iroot(BigInteger.valueOf(k), BigInteger.valueOf(n));
+    }
+
+    /**
+     * find the kth root of figure n
+     * @param k the root
+     * @param n the figure
+     * @return the kth root of figure n
+     */
+    public BigInteger iroot(final BigInteger k, final BigInteger n) {
+        //initial guess
+        final var k1 = k.subtract(ONE);
+        var s = n.add(ONE);
+        var u = n;
+        while (u.compareTo(s) < 0) {
+            s = u;
+            u = u.multiply(k1).add(n.divide(u.pow(k1.intValue()))).divide(k);
+        }
+        return s;
     }
 }
