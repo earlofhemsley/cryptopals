7d0c2eb1cdf614c3c3993d1677b841ddc6ac4d84
Landon Hemsley <landonhemsley@proton.me>
2024-07-08 21:31:23 -0600

    consolidate implementation for 12 & 14 into single function

--- src/main/java/cryptopals/tool/sec02/Challenge12Tool.java
+++ src/main/java/cryptopals/tool/sec02/Challenge12Tool.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class Challenge12Tool {
@@ -33,6 +35,72 @@ public class Challenge12Tool {
         return filled.length - empty.length;
     }
 
+    /**
+     * @param oracle           the oracle function of concern. takes in a byte array, returns a byte array
+     * @param numPrefixBlocks  number of prefix blocks. used to determine an offset
+     * @param prefixBuffer     a buffer that will push the beginning of the hacker input to the start of a block
+     * @param numMysteryBlocks the number of mystery blocks you're trying to extract
+     * @param blockSize        the block size
+     * @return the extracted message
+     */
+    static byte[] performExtraction(Function<byte[], byte[]> oracle, int numPrefixBlocks, byte[] prefixBuffer, int numMysteryBlocks, int blockSize) {
+        byte[] extracted = new byte[0];
+
+        Map<Integer, byte[]> targets = new HashMap<>();
+        for (int k = 0; k < numMysteryBlocks; k++) {
+            int o = k + numPrefixBlocks; // o is our offset to the block we are interrogating
+
+            byte[] block = new byte[blockSize];
+            for (int i = 0; i < blockSize; i++) {
+
+                final int len = blockSize - i - 1;
+                final byte[] filler = ByteArrayUtil.concatenate(prefixBuffer, new byte[len]);
+
+                //we can save these targets because
+                // recomputing them on subsequent executions results in the same outcome
+                // because ECB is deterministic. waste not cpu cycles
+                var fullTarget = targets.computeIfAbsent(len, l -> oracle.apply(filler));
+
+                var targetBlock = ByteArrayUtil.sliceByteArray(fullTarget, o * blockSize, blockSize);
+
+                byte[] hackerInput = ByteArrayUtil.concatenate(
+                        filler, //rounds out the prefix block, then gives us ( blockSize - i ) bytes in the next one
+                        ByteArrayUtil.sliceByteArray(extracted, 0, extracted.length), // anything we got from previous rounds
+                        ByteArrayUtil.sliceByteArray(block, 0, i), // anything we got so far on this round
+                        new byte[1] //one more empty byte to round out the block
+                );
+
+                //validate that the hacker input less the prefix buffer is a multiple of the block size
+                if ((hackerInput.length - prefixBuffer.length) % blockSize != 0) {
+                    throw new CryptopalsException("the hackerInput didn't fill out a full block");
+                }
+
+                boolean found = false;
+                for (int j = 0; j < 256; j++) {
+                    hackerInput[hackerInput.length - 1] = (byte) j;
+                    var result = oracle.apply(hackerInput);
+                    var subjectBlock = ByteArrayUtil.sliceByteArray(result, o * blockSize, blockSize);
+                    if (Arrays.equals(targetBlock, subjectBlock)) {
+                        block[i] = (byte) j;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    if (k == numMysteryBlocks - 1) { //we're done
+                        block = ByteArrayUtil.sliceByteArray(block, 0, i - 1);
+                        break;
+                    } else { // we're in trouble
+                        throw new RuntimeException(String.format("could not find the match. k=%d, numMysteryBlocks=%d, offset=%d", k, numMysteryBlocks, o));
+                    }
+                }
+            }
+            extracted = ByteArrayUtil.concatenate(extracted, block);
+        }
+
+        return extracted;
+    }
+
     public static byte[] extractTheMysteryString() {
         final int blockSize = determineBlockSize(Challenge12Oracle::speakProphecy);
         if (blockSize != 16) {
@@ -45,55 +113,9 @@ public class Challenge12Tool {
         boolean ecbDetected = ECBDetector.isECBEncrypted(ecbDetectionArray, blockSize);
         assert ecbDetected;
 
-        byte[] extracted = new byte[0];
         byte[] empty = Challenge12Oracle.speakProphecy(new byte[0]);
         int nBlocks = empty.length / blockSize;
 
-        for (int k = 0; k < nBlocks; k++) {
-            byte[] block = new byte[blockSize];
-            for (int i = 1; i <= blockSize; i++) {
-                final byte[] filler = new byte[blockSize - i];
-                Arrays.fill(filler, (byte) 'A');
-
-                byte[] fullTarget = Challenge12Oracle.speakProphecy(filler);
-                byte[] targetSegment = ByteArrayUtil.sliceByteArray(fullTarget, k*blockSize, blockSize);
-
-                byte[] seed = ByteArrayUtil.concatenate(
-                        filler,
-                        ByteArrayUtil.sliceByteArray(extracted, 0, k*blockSize),
-                        ByteArrayUtil.sliceByteArray(block, 0, i-1)
-                );
-
-                log.debug("seed (length): {} ({})", new String(seed), seed.length);
-                if (seed.length % blockSize != blockSize - 1) {
-                    throw new CryptopalsException("the seed wasn't one byte short of a round block");
-                }
-
-                byte[] hackerInput = new byte[seed.length + 1];
-                System.arraycopy(seed, 0, hackerInput, 0, seed.length);
-                boolean found = false;
-                for (int j = 0; j < 256; j++) {
-                    hackerInput[hackerInput.length - 1] = (byte) j;
-                    var result = Challenge12Oracle.speakProphecy(hackerInput);
-                    var subjectSegment = ByteArrayUtil.sliceByteArray(result, k*blockSize, blockSize);
-                    if (Arrays.equals(targetSegment, subjectSegment)) {
-                        block[i - 1] = (byte) j;
-                        found = true;
-                        break;
-                    }
-                }
-                if (!found) {
-                    if (k == nBlocks - 1) {
-                        block = ByteArrayUtil.sliceByteArray(block, 0, i - 2);
-                        break;
-                    } else {
-                        throw new RuntimeException("could not find the matching target block");
-                    }
-                }
-            }
-            extracted = ByteArrayUtil.concatenate(extracted, block);
-        }
-
-        return extracted;
+        return performExtraction(Challenge12Oracle::speakProphecy, 0, new byte[0], nBlocks, blockSize);
     }
 }
--- src/main/java/cryptopals/tool/sec02/Challenge14Tool.java
+++ src/main/java/cryptopals/tool/sec02/Challenge14Tool.java
@@ -5,8 +5,6 @@ import cryptopals.utils.ByteArrayUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 public class Challenge14Tool {
@@ -62,65 +60,6 @@ public class Challenge14Tool {
         log.debug("numMysteryBlocks: {}", numMysteryBlocks);
         log.debug("numTotalBlocks: {}", numTotalBlocks);
 
-        // at this point, I can basically do what I did in challenge 12, except the starting block is the
-        // first one after the prefix ends. So, there's an offset to account for.
-        // TODO: take this part and put it in a function that can be referenced from both challenge 12 and 14
-        //  probably put it in the challenge 12 tool and reference it here like I did with the determine block size function
-        byte[] extracted = new byte[0];
-
-        Map<Integer, byte[]> targets = new HashMap<>();
-        for (int k = 0; k < numMysteryBlocks; k++) {
-            int o = k + numPrefixBlocks; // o is our offset to the block we are interrogating
-
-            byte[] block = new byte[blockSize];
-            for (int i = 0; i < blockSize; i++) {
-
-                final int len = blockSize - i - 1;
-                final byte[] filler = ByteArrayUtil.concatenate(prefixBuffer, new byte[len]);
-
-                //we can save these targets because
-                // recomputing them on subsequent executions results in the same outcome
-                // because ECB is deterministic. waste not cpu cycles
-                var fullTarget = targets.computeIfAbsent(len, (l) ->
-                        Challenge14Oracle.speakProphecy(filler));
-
-                var targetBlock = ByteArrayUtil.sliceByteArray(fullTarget, o * blockSize, blockSize);
-
-                byte[] hackerInput = ByteArrayUtil.concatenate(
-                        filler, //rounds out the prefix block, then gives us ( blockSize - i ) bytes in the next one
-                        ByteArrayUtil.sliceByteArray(extracted, 0, extracted.length), // anything we got from previous rounds
-                        ByteArrayUtil.sliceByteArray(block, 0, i), // anything we got so far on this round
-                        new byte[1] //one more empty byte to round out the block
-                );
-
-                //validate that the hacker input less the prefix buffer is a multiple of the block size
-                if ((hackerInput.length - prefixBuffer.length) % blockSize != 0) {
-                    throw new CryptopalsException("the hackerInput didn't fill out a full block");
-                }
-
-                boolean found = false;
-                for (int j = 0; j < 256; j++) {
-                    hackerInput[hackerInput.length - 1] = (byte) j;
-                    var result = Challenge14Oracle.speakProphecy(hackerInput);
-                    var subjectBlock = ByteArrayUtil.sliceByteArray(result, o * blockSize, blockSize);
-                    if (Arrays.equals(targetBlock, subjectBlock)) {
-                        block[i] = (byte) j;
-                        found = true;
-                        break;
-                    }
-                }
-                if (!found) {
-                    if (k == numMysteryBlocks - 1) { //we're done
-                        block = ByteArrayUtil.sliceByteArray(block, 0, i - 1);
-                        break;
-                    } else { // we're in trouble
-                        throw new RuntimeException(String.format("could not find the match. k=%d, numMysteryBlocks=%d, o=%d, numTotalBlocks=%d", k, numMysteryBlocks, o, numTotalBlocks));
-                    }
-                }
-            }
-            extracted = ByteArrayUtil.concatenate(extracted, block);
-        }
-
-        return extracted;
+        return Challenge12Tool.performExtraction(Challenge14Oracle::speakProphecy, numPrefixBlocks, prefixBuffer, numMysteryBlocks, blockSize);
     }
 }
